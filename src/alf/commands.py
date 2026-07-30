8c840c99c107c8c5323d62b3522360a94ece5f7f
Peter Ball <pierreworking@gmail.com>
2026-07-30 13:20:43 +0100

    Add hello command

--- src/alf/commands.py
+++ src/alf/commands.py
@@ -5,6 +5,22 @@ ALF command dispatcher.
 from .system import get_system_report
 
 
+def status_command():
+    print()
+    print(get_system_report())
+    print()
+
+def hello_command():
+    print()
+    print("Hello Peter.")
+    print()
+
+commands = {
+    "status": status_command,
+    "hello" : hello_command
+}
+
+
 def run(command: str):
     """
     Execute an ALF command.
@@ -12,10 +28,8 @@ def run(command: str):
     Returns True if the command was recognised.
     """
 
-    if command == "status":
-        print()
-        print(get_system_report())
-        print()
+    if command in commands:
+        commands[command]()
         return True
 
     return False
