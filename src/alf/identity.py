b768b0b937eec35394132076b98f74a1459490d4
Peter Ball <pierreworking@gmail.com>
2026-08-10 15:57:29 +0100

    refactor: derive version from package metadata

--- src/alf/identity.py
+++ src/alf/identity.py
@@ -3,6 +3,7 @@ ALF identity management.
 """
 
 import tomllib
+from importlib.metadata import version as package_version
 
 from .capabilities import discover_capabilities
 from .paths import get_data_directory
@@ -12,7 +13,11 @@ def get_identity():
     identity_file = get_data_directory() / "identity.toml"
 
     with open(identity_file, "rb") as file:
-        return tomllib.load(file)
+        identity = tomllib.load(file)
+
+    identity["version"] = package_version("alf")
+
+    return identity
 
 
 def get_about_information():
