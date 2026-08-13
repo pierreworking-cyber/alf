95b5dadd80982743af4363250c7bacf8b6d6ad41
Peter Ball <pierreworking@gmail.com>
2026-08-13 19:49:53 +0100

    main: support conventional help flags

--- src/alf/main.py
+++ src/alf/main.py
@@ -5,7 +5,7 @@ ALF - Peter's local computing companion.
 
 """
 
-from .commands import run, show_status
+from .commands import help_command, run, show_status
 from .presentation import (
     error,
     info,
@@ -23,6 +23,10 @@ def main():
     import sys
 
     if len(sys.argv) > 1:
+        if sys.argv[1] in ("--help", "-h"):
+            help_command()
+            return
+
         command = sys.argv[1]
         arguments = sys.argv[2:]
         if run(command, arguments):
